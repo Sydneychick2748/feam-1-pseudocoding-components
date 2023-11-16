7fbf22dbaf6a71444237b8297bff0804ecc1bcc2
ana <ana@anniecannons.com>
2023-11-16 12:59:44 -0800

    commitng the pseudocoidng

--- Assignment.js
+++ Assignment.js
@@ -1,58 +1,59 @@
-
 // Pseudocode in your own words the steps you would take to complete each of the component requirements below:
 
-
-
 // Component Requirements:
-    // Build a Login component that accepts the users email and password.
-    
-// Pseudocode:
-    // 
-    // 
-    // 
-
-
-
-
-
-
-
-// Component Requirements:
-    // Build a side menu component that has 5 buttons.
+// Build a Login component that accepts the users email and password.
 
 // Pseudocode:
-    // 
-    // 
-    // 
 
-
-
-
-
-
+// the user needs to be able to type in a user name ,
+//email
+//and password
+// then you should store the information that the user inputs
+// also include  validation so when the user types in the text all felids are complete
+//then the user needs to submit the input with a button
 
 // Component Requirements:
-    // Build a filter component that can filter chart data.
+// Build a side menu component that has 5 buttons.
 
 // Pseudocode:
-    // 
-    // 
-    // 
+//  make a component in the file
+// add the card that has a button
+//  pass in the import to the parent folder
+// you will want 5 cards so make 5 instances of the card i the jsx in the parent folder
+//Attach click event handlers to the buttons to handle user interactions.
+// Add labels or icons to represent each button's functionality.
+//Style the buttons
 
+// Component Requirements:
+// Build a filter component that can filter chart data.
 
-
+// Pseudocode:
+// make a component that is in the file structure in vscode
+// add a list for the items  you want to be filtered from your chart
+// Implement a click event handler for each item to update the selected filter in the state.
+// Use the selected filter option to filter the chart data.
+// Display the filtered data on the chart.
+// then  reset or clear the filters.
+// Style the filter component
 
 
 
 
 // Component Requirements:
-    // Build a profile component where users can add and edit their profile information.
+// Build a profile component where users can add and edit their profile information.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-    // 
+//create a profile component in the files
+// set up a list of information you want the user to input like name , address ,email 
+// make a input felid that the user can input the information 
+//once the user fills out all the input 
+// let the user click a button to save the information 
+// Decide where and how the user's profile information will be stored.
+// implement a way to keep the users input secure 
+// use validation to make sure the user has filled out all felids 
+//Fetch and display existing user profile information if available.
+//use an edit button to allow the user to edit there information and save the new  information 
+//style the profile page 
 
 
 
@@ -60,10 +61,12 @@
 
 
 // Component Requirements:
-    // Build a bar chart component that displays 2 sets of data from the backend.
+// Build a bar chart component that displays 2 sets of data from the backend.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-    // 
+// Create a BarChart component in your file structure.
+// store the data that is needed for the chart that you have obtained from the backend or use an api to fetch the data if needed
+// then try using a chart library to inclement making charts 
+//import the library to your react site 
+//Render the bar chart with the provided data.
+// Style the chart for better visualization.
